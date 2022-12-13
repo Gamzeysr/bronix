4263f6071d1a8d6673a8c042e874876eeaad37a1
Gamze <gamzeyasar35@icloud.com>
2022-12-13 20:52:42 +0300

    best practice

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,14 +4,14 @@ import FooterLogo from "../../assets/footer-logo.png";
 
 const Footer = () => {
     return (
-        <div className={FooterStyle.containerfooter}>
-            <div className={FooterStyle.leftlogo}>
+        <div className={FooterStyle["containerfooter"]}>
+            <div className={FooterStyle["leftlogo"]}>
                 <img src={FooterLogo} alt="logo" />
                 <p>All rights reserved.</p>
             </div>
-            <div className={FooterStyle.menu} >
-                <div className={FooterStyle.col}>
-                    <div className={FooterStyle.title}>Menu</div>
+            <div className={FooterStyle["menu"]}>
+                <div className={FooterStyle["col"]}>
+                    <div className={FooterStyle["title"]}>Menu</div>
                     <ul>
                         <li>About Us</li>
                         <li>News</li>
@@ -19,15 +19,15 @@ const Footer = () => {
                         <li>Contact</li>
                     </ul>
                 </div>
-                <div className={FooterStyle.col}>
-                    <div className={FooterStyle.title}>Legal</div>
+                <div className={FooterStyle["col"]}>
+                    <div className={FooterStyle["title"]}>Legal</div>
                     <ul>
                         <li>Privacy Policy</li>
                         <li>Terms & Conditions</li>
                     </ul>
                 </div>
-                <div className={FooterStyle.col}>
-                    <div className={FooterStyle.title}>Social</div>
+                <div className={FooterStyle["col"]}>
+                    <div className={FooterStyle["title"]}>Social</div>
                     <ul>
                         <li>Discord</li>
                         <li>Linkedin</li>
--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -1,16 +1,15 @@
-
 import NavbarStyle from "./navbar.module.css";
 
 
 
 const Navbar = () => {
     return (
-        <ul className={NavbarStyle.ulContainer}>
-            <li className={NavbarStyle.navlistitem}>About Us</li>
-            <li className={NavbarStyle.navlistitem}>Games</li>
-            <li className={NavbarStyle.navlistitem}> News</li>
-            <li className={NavbarStyle.navlistitem}>Career</li>
-            <li className={NavbarStyle.navlistitem}>Contact</li>
+        <ul className={NavbarStyle["ulContainer"]}>
+            <li className={NavbarStyle["navlistitem"]}>About Us</li>
+            <li className={NavbarStyle["navlistitem"]}>Games</li>
+            <li className={NavbarStyle["navlistitem"]}> News</li>
+            <li className={NavbarStyle["navlistitem"]}>Career</li>
+            <li className={NavbarStyle["navlistitem"]}>Contact</li>
         </ul>
 
     );
