08f06f2b2816a1a6c837014cef3e2d707fc2ec1b
Gamze <gamzeyasar35@icloud.com>
2022-12-13 22:21:34 +0300

    contactus

--- src/components/contactus/ContactUs.jsx
+++ src/components/contactus/ContactUs.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
 import ContactUsStyle from "./contactus.module.css";
+import contactUsImg from "../../assets/contact-us.png";
+
 
 
 const ContactUs = () => {
     return (
-        <div>ContactUs</div>
+        <div className={ContactUsStyle["containerContact"]}>
+            <div>
+                <h2 className={ContactUsStyle["title"]}>Contact Us</h2>
+                <img className={ContactUsStyle["img"]} src={contactUsImg} alt="Contact-Us-Img" />
+            </div>
+            <div>
+                <div>
+                    <input type="text" placeholder='  Name' />
+                    <input type="email" placeholder='  Email' />
+                    <input type="text" placeholder='  Your message' />
+                </div>
+                <button className={ContactUsStyle["send-button"]} type="send">Send</button>
+            </div>
+        </div>
     )
 }
 
