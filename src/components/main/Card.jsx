9e4d6e3374669317d73d8794a078dc7a6a3902a5
Gamze <gamzeyasar35@icloud.com>
2022-12-14 23:00:46 +0300

    data

--- src/components/main/Card.jsx
+++ src/components/main/Card.jsx
@@ -1,9 +1,31 @@
 
 import CardStyle from "./card.module.css";
-
+import Icon from "../../assets/icon.png";
 const Card = () => {
     return (
-        <div>Card</div>
+        <div className={CardStyle["cardContainer"]}>
+
+            {/* <img className={CardStyle["Img"]} src={image} alt="img" /> */}
+            <div className={CardStyle["cardContent"]}>
+                <h3 className={CardStyle["cardTitle"]}>BASLIK</h3>
+                <p className={CardStyle[""]}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Bibendum
+                    vitae dictumst sit vitae, mi imperdiet sit. Lectus eleifend aliquam
+                    nibh mauris, pretium. Lectus magnis lorem massa urna felis porta.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Bibendum
+                    vitae dictumst sit vitae, mi imperdiet sit. Lectus eleifend aliquam
+                    nibh mauris, pretium. Lectus magnis lorem massa urna felis porta.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Bibendum
+                    vitae dictumst sit vitae, mi imperdiet sit. Lectus eleifend aliquam
+                    nibh mauris, pretium. Lectus magnis lorem massa urna.</p>
+                <div>
+                    <p>text</p>
+                    <div>
+                        <img src={Icon} alt="icon" />
+                        <p>Beğen</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
---
+++ src/components/pagination/Pagination.jsx
@@ -0,0 +1,9 @@
+import Pagination from "./Pagination.module.css";
+
+const pagination = () => {
+    return (
+        <div>pagination</div>
+    )
+}
+
+export default pagination
