fd9f7bd56795cba810e11871a47a6ad3fc1030c0
Gamze <gamzeyasar35@icloud.com>
2022-12-15 10:10:00 +0300

    data deleted

--- src/components/main/Card.jsx
+++ src/components/main/Card.jsx
@@ -8,7 +8,7 @@ const Card = () => {
             {/* <img className={CardStyle["Img"]} src={image} alt="img" /> */}
             <div className={CardStyle["cardContent"]}>
                 <h3 className={CardStyle["cardTitle"]}>BASLIK</h3>
-                <p className={CardStyle[""]}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Bibendum
+                <p className={CardStyle["cardText"]}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Bibendum
                     vitae dictumst sit vitae, mi imperdiet sit. Lectus eleifend aliquam
                     nibh mauris, pretium. Lectus magnis lorem massa urna felis porta.
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Bibendum
@@ -17,7 +17,7 @@ const Card = () => {
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Bibendum
                     vitae dictumst sit vitae, mi imperdiet sit. Lectus eleifend aliquam
                     nibh mauris, pretium. Lectus magnis lorem massa urna.</p>
-                <div>
+                <div >
                     <p>text</p>
                     <div>
                         <img src={Icon} alt="icon" />
